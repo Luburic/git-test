2eec3d1b3c6a2a2fd16063909f2acd6180626643
Nikola Luburic <luburic91@gmail.com>
2021-03-15 11:41:25 +0100

    feat: Expands product to include price and quantity.

--- src/Store.java
+++ src/Store.java
@@ -1,27 +1,39 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Store {
 
     public static void main(String[] args) {
-        ArrayList<String> inventory = new ArrayList<>();
-        inventory.add("Tastatura XZ");
-        inventory.add("Tastatura YY");
-        inventory.add("Televizor LG KY");
-        inventory.add("Televizor Samsung X1");
-        inventory.add("Ves masina 12");
-        inventory.add("Ves masina X4");
-        inventory.add("Pegla TT");
+        //Za svaki proizvod ukljuci i njegovu cenu i kolicinu
+
+        ArrayList<HashMap<String, String>> inventory = new ArrayList<>();
+
+        inventory.add(createProduct("Tastatura XZ", "3000", "3"));
+        inventory.add(createProduct("Tastatura YY", "3999", "5"));
+        inventory.add(createProduct("Televizor LG KY", "25000", "3"));
+        inventory.add(createProduct("Televizor Samsung X1", "15000", "4"));
+
 
         System.out.println("Unesi parametar pretrage:");
         Scanner scanner = new Scanner(System.in);
         String searchText = scanner.nextLine();
 
-        for (String p : inventory) {
-            if (p.toUpperCase().contains(searchText.toUpperCase())) {
-                System.out.println(p);
+        for (HashMap<String, String> p : inventory) {
+            if (p.get("name").toUpperCase().contains(searchText.toUpperCase())) {
+                System.out.println("\n\nName: " + p.get("name"));
+                System.out.println("\nPrice: " + p.get("price"));
+                System.out.println("\nQuantity: " + p.get("quantity"));
             }
         }
 
+    }
+
+    private static HashMap<String, String> createProduct(String name, String price, String quantity) {
+        HashMap<String, String> product = new HashMap<>();
+        product.put("name", name);
+        product.put("price", price);
+        product.put("quantity", quantity);
+        return product;
     }
 }
